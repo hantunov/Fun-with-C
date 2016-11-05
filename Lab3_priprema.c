a2532a489333383c15a7f5cd16fe2ad4a2c782ef
hantunov <hrvoje.antunovic@gmail.com>
2016-11-05 01:50:56 +0100

    Update Lab3_priprema.c

--- Lab3_priprema.c
+++ Lab3_priprema.c
@@ -3,13 +3,21 @@
 #include <math.h>
 #define MAX 100
 
-/*Jedan vaš prijatelj, popularno zvan "Giza", odlučio je probati zaraditi na pretvaranju valuta. Inicijalno, on raspolaže sa 100,00 kn. Svaki dan, pogleda tečaj za promjenu EUR <-> KN, i ako je tečaj najpovoljniji (za njega) koji je on do sad vidio, pretvorit će sve novce jedne valute u drugu. 
+/*Jedan vaš prijatelj, popularno zvan "Giza", odlučio je probati zaraditi na pretvaranju valuta. Inicijalno, on raspolaže
+sa 100,00 kn. Svaki dan, pogleda tečaj za promjenu EUR <-> KN, i ako je tečaj najpovoljniji (za njega) koji je on do sad vidio,
+pretvorit će sve novce jedne valute u drugu. 
 
-Vaš zadatak je napisati program koji će simulirati ponašanje našeg Gize, i nakon što prođe N dana ispisati koliko kuna ili eura ima Giza.
+Vaš zadatak je napisati program koji će simulirati ponašanje našeg Gize, i nakon što prođe N dana ispisati koliko
+kuna ili eura ima Giza.
 
-Učitava se prirodni broj N, koji označava koliko dana Giza mijenja novce, a u svakom retku nalaze se dva decimalna broja k i p sa 5 decimala koji označavaju kupovni i prodajni tečaj za EUR:
-Ukoliko Giza ima 100,00 kn, a kupovni tečaj za EUR je 7,50000; Giza će taj dan završiti s 13,33 € (ukoliko, prema pravilima zadatka, odluči promijeniti novce). Primjerice, kad idući put bude imao kune, promijenit će samo ako je kupovni tečaj manji od 7,50000 (tada je tečaj povoljniji)
-Ukoliko Giza ima 20,00 €, a prodajni tečaj za EUR je 7,40000; Giza će taj dan završiti sa 148,00 kn (uvijek se zaokružuje na 2 decimale). Primjerice, kad idući put bude imao eure, promijenit će samo ako je prodajni tečaj veći od 7,40000 (tada mu je tečaj povoljniji)*/
+Učitava se prirodni broj N, koji označava koliko dana Giza mijenja novce, a u svakom retku nalaze se dva decimalna broja
+k i p sa 5 decimala koji označavaju kupovni i prodajni tečaj za EUR:
+Ukoliko Giza ima 100,00 kn, a kupovni tečaj za EUR je 7,50000; Giza će taj dan završiti s 13,33 €
+(ukoliko, prema pravilima zadatka, odluči promijeniti novce). Primjerice, kad idući put bude imao kune,
+promijenit će samo ako je kupovni tečaj manji od 7,50000 (tada je tečaj povoljniji)
+Ukoliko Giza ima 20,00 €, a prodajni tečaj za EUR je 7,40000; Giza će taj dan završiti sa 148,00 kn
+(uvijek se zaokružuje na 2 decimale). Primjerice, kad idući put bude imao eure, promijenit će samo ako je
+prodajni tečaj veći od 7,40000 (tada mu je tečaj povoljniji)*/
 
 
 int main()
