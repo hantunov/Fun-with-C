2aa4a28ad37127ca7c34aa580c089a5ca11cac4f
Hrvoje Antunović <hrvoje.antunovic@gmail.com>
2018-07-30 02:18:19 +0200

    Update Vjezba_00.c

--- Algoritmi i strukture/00_IzracunFaktorijela/Vjezba_00.c
+++ Algoritmi i strukture/00_IzracunFaktorijela/Vjezba_00.c
@@ -1,22 +1,35 @@
+/*
+Napisati potpuno funkcionalni program u programskom jeziku C koji računa faktorijel broja n (n!).
+
+Npr.
+n=1 → 1! = 1
+n=2 → 2! = 1 * 2 = 2
+n=3 → 3! = 1 * 2 * 3 = 6
+Itd.
+
+Korisnik mora unijeti cijeli broj n, te se potom fatorijel računa u petlji. Ispisuje se iznos faktorijela u svakom koraku
+petlje. Ako je broj koraka petlje već od 10 petlja se prekida i ispisuje se odgovarajuća poruka.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int n = 0, i, faktorijel = 1;
+    int i, n, result = 1;
 
-    scanf("%d", &n);
+    do{
+        printf("Unesite broj:\n");
+        scanf("%d", &n);
+    }while(n<1);
 
-    for(i = 1; i<=n; i++){
-
-        faktorijel *= i;
-        printf("%d! = %d\n", i, faktorijel);
-
-        if(i>=10){
-            printf("Nemoguce racunati faktorijele vece od 10!\n");
+    for(i=1; i<=n; i++){
+        if(i>10){
+            printf("Broj je prevelik, prekidam!\n");
             break;
         }
+        printf("%d! = ", i);
+        result *= i;
+        printf("%d\n", result);
     }
-
-    return 0;
 }
